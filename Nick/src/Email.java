3e6637a02d2302e64bea01e5cab6265b99284d31
kozln20 <kozln20@wfu.edu>
2022-11-18 15:13:59 -0500

    My second commit.

---
+++ Nick/src/Email.java
@@ -0,0 +1,7 @@
+public class Email {
+
+    public static void main(String[] args) {
+        Email eml = new Email("John","Doe");
+    }
+
+}
--- Nick/src/EmailApplication.java
+++ Nick/src/EmailApplication.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.lang.Math.*;
 public class EmailApplication {
     private String firstName;
@@ -7,17 +8,39 @@ public class EmailApplication {
     private int mailboxCap;
     private String emailAlt;
 
-    public Email(String firstName, String lastName); {
-
-
+    public EmailApplication (String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("Email successfully created.");
+        System.out.println("Email successfully created: " + this.firstName + this.lastName);
+
+        this.dep = setDepartment();
+        System.out.println("Department: " + this.dep);
+    }
+
+    private String setDepartment() {
+        System.out.println("Department Codes\n1 for Sales\n2 for Accounting\n3 for Development\n0 for none\n Enter your department code: );
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) { return "sales"; }
+        else if (depChoice == 2) { return "accounting"; }
+        else if (depChoice == 3) { return "development"; }
+                else { return ""; }
+    }
+
+
+        this.password = passwordGenerator(defaultPasswordLength); {
+        System.out.println("Password: " + this.password);
     }
 
     private String passwordGenerator(int length) {
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
-        char[] password =
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++){
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
 
     }
 }
