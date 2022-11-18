bb8d29e12f83a80d8bb92503c6fa90f66b9ad563
kozln20 <kozln20@wfu.edu>
2022-11-18 14:47:09 -0500

    My first commit.

---
+++ Nick/src/EmailApplication.java
@@ -0,0 +1,23 @@
+import java.lang.Math.*;
+public class EmailApplication {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String dep;
+    private int mailboxCap;
+    private String emailAlt;
+
+    public Email(String firstName, String lastName); {
+
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email successfully created.");
+    }
+
+    private String passwordGenerator(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password =
+
+    }
+}
