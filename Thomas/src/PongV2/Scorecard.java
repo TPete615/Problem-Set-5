0b2a68af4d15697a0f18b33c75b2164136d0b827
TPete615 <petetj21@wfu.edu>
2022-11-30 12:16:35 -0500

    Seventh commit. Added rough high score system (not complete)

--- Thomas/src/PongV2/Scorecard.java
+++ Thomas/src/PongV2/Scorecard.java
@@ -14,10 +14,18 @@ public class Scorecard {
     public void incrementScore() {
         this.score += 1;
     }
+    private int prevScore = score;
+    private int highScore = 0;
     public void draw(Graphics g){
         g.setColor(Color.white);
         g.drawString("Score: " + score, 300, 200);
+        if(prevScore > highScore) {
+            highScore = prevScore;
+            g.drawString("High Score: " + highScore, 300, 100);
+        }
+        else if(highScore >= prevScore) {
+            g.drawString("High Score: " + highScore, 300, 100);
+        }
     }
-
 }
 
